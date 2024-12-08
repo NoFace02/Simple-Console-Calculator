f9f999fc5682cb91d9384d269ef2495d7e4fba22
Sans3108 <dragnerobertleonard13@gmail.com>
2024-12-08 16:45:34 +0200

    Added rerun with result

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Calculator
 {
@@ -69,7 +70,26 @@ namespace Calculator
 
 
             // Getting numbers
-            double num1 = GetDoubleFromStdin("What is the first number?\n> ");
+            double num1 = 0;
+
+            if (args.Length > 0)
+            {
+                try
+                {
+                    Console.WriteLine($"Your first number was received from a previous run: {args[0]}");
+                    num1 = Convert.ToDouble(args[0]);
+                } catch
+                {
+                    Console.WriteLine("Something went wrong, please try again.");
+                    Console.WriteLine("Press any key to exit...\n");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+            } else
+            {
+                num1 = GetDoubleFromStdin("What is the first number?\n> ");
+            }
+
             double num2 = GetDoubleFromStdin("What is the second number?\n> ");
 
             // Define supported operations
@@ -130,8 +150,19 @@ namespace Calculator
 
             // Print the result and wait for exit
             Console.WriteLine($"{num1} {chosenOperation.Item3} {num2} = {result}\n");
-            Console.WriteLine("Press any key to exit...\n");
-            Console.ReadKey();
+
+            char[] opts = { 'y', 'n' };
+            char shouldRestart = GetCharFromStdin("Would you like to use the result for another operation? y/n\n> ", opts);
+
+            if (shouldRestart == 'y')
+            {
+                Process.Start(Environment.GetCommandLineArgs()[0], result.ToString());
+                Environment.Exit(0);
+            } else
+            {
+                Console.WriteLine("Press any key to exit...\n");
+                Console.ReadKey();
+            }
         }
     }
 }
